1ce30ca9686ad729a13b29575517b5ed0806f906
hsamvel <samvelharutyunyan645@gmail.com>
2020-10-14 02:06:34 +0400

    first homework by git

---
+++ homework_from_slide.py
@@ -0,0 +1,13 @@
+#homework_2
+#for i in range(1,5):
+    #for j in range(1,i+1):
+        #if j < i:
+            #print(i,end=' ')
+        #else:
+            #print(i,end='\n')
+
+#homework_1
+#for i in range(2,101,1):
+    #for j in range(i+1,101,1):
+        #if i ** j == j ** i:
+            #print(i,j,i**j)   
