2d15ca8fd37760da2eb4e1c98cdeed3011a101e3
Samvel Harutyunyan <72821052+hsamvel@users.noreply.github.com>
2020-11-07 13:52:06 +0400

    Update homework8_2(Narek).py

--- homework8_2(Narek).py
+++ homework8_2(Narek).py
@@ -2,7 +2,15 @@ list_1 = [9,5,8,5,20,1,2,-3,-2,-1,0]
 list_1.sort()
 list_1.reverse()
 list_2 = list_1[0:3]
-count = 1
-for el in list_2:
-    count *= el
-print(count)
+list_3 = list_1[len(list_1)-2:len(list_1)]
+list_3_count=1
+for el in list_3:
+    list_3_count *= el
+list_3_count *=list_1[0]
+list_2_count = 1
+for elem in list_2:
+    list_2_count*=elem
+if list_3_count > list_2_count:
+    print(list_3_count)
+else:
+    print(list_2_count)
