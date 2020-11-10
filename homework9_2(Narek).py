33a47fab66614ba5b2dd20c82a521025693234e0
Samvel Harutyunyan <72821052+hsamvel@users.noreply.github.com>
2020-11-10 16:22:05 +0400

    Update homework9_2(Narek).py

--- homework9_2(Narek).py
+++ homework9_2(Narek).py
@@ -2,9 +2,14 @@ string_1 = '(bar)'
 
 
 def reverse_string(string_1):
-    string_1 = string_1.replace(string_1[string_1.rfind('('):string_1.rfind(')')+1],string_1[string_1.rfind(')')-1:string_1.rfind('('):-1])
-    if string_1.count(')') >= 1:
+    if string_1.count(')') == 1:
+        string_1 = string_1.replace(string_1[string_1.rfind('('):string_1.rfind(')')+1],string_1[string_1.rfind(')')-1:string_1.rfind('('):-1])
+    if string_1[string_1.find(')')]==string_1[string_1.find(')')+1]:
+        string_1 = string_1.replace(string_1[string_1.rfind('('):string_1.find(')')+1],string_1[string_1.find(')')-1:string_1.rfind('('):-1])
         return reverse_string(string_1)
-    return string_1
+    string_1 = string_1.replace(string_1[string_1.rfind('('):string_1.rfind(')')+1],string_1[string_1.rfind(')')-1:string_1.rfind('('):-1])
+    if ')' not in string_1:
+        return string_1
+    return reverse_string(string_1)
 
 print(reverse_string(string_1))
