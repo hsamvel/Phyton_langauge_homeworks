2c9b4fcd3b4137bd39094501f3e2269ea685fcb5
Samvel Harutyunyan <72821052+hsamvel@users.noreply.github.com>
2020-11-17 17:24:48 +0400

    Update homework11_2(Narek).py

--- homework11_2(Narek).py
+++ homework11_2(Narek).py
@@ -1,10 +1,10 @@
-list_1 =[4, 2, 2, 5, 3]
+list_1 =[4, 2, 1, 5, 3]
 
 
 def sum_of_pairs(list_1,number):
     for i in range(len(list_1)):
         if number - list_1[i] in list_1:
-            if number - list_1[i] == list_1[i] and list_1.count(list_1[i]) != 1:
+            if number - list_1[i] == list_1[i] and list_1[i] in list_1[i+1:]:
                 return number - list_1[i] , list_1[i]
             if number - list_1[i] != list_1[i]:
                 return number - list_1[i],list_1[i]
